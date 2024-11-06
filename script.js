b848906920708217b9aae81db9c1b7379922bc2e
Регина911 <shaybakovaregina08@gmail.com>
2024-11-06 17:47:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+//1 task
+// let name = prompt('Как ваше имя?')
+// alert('Привет ' +  name)
+
+//2 task
+// const year = 2024
+// let birthYear = prompt('Год Вашего рождения:')
+// let age = year - birthYear
+// alert(age)
+
+//3 task
+// let side = prompt('input A side of the square')
+// alert( side * 4)
+
+
+// //4 task
+// PI = 3.14
+// let radius = prompt('Введите радиус окружности')
+// function (S)
+// S = PI × r2
+// let square = Math.PI * radius **2;
+// alert( 'Площадь лкружности: '* square)
+
+// let radius = prompt('input radius of the circle')
+//     alert(Math.PI*Math.pow(radius,2))
+
+
+// //5 task  
+// let Time = prompt('Введите расстояние между городами в км:')
+// let Dist = prompt('За какое время вы хотите преодолеть это расстояние в часах?')
+// alert(Dist/Time +' km/h') 
+
+// //6 task 
+// const kurs = 0,84
+// let Dollars =prompt('Введите количество долларов для обмена::')
+// alert((Dollars*kurs).toFixed(2) +' Euro')
+
+// const rate = Number(prompt('Введите текущий курс доллара по отношению к евро: '));
+// let dollar = Number(prompt('Введите количество долларов для обмена: '));
+// alert("По текущему курсу при обмене вы получите " + dollar * rate + " евро");
+
+// //7 task 
+// const GB_MB = 1024
+// let Vol = prompt('Укажите объем вашей флешкки в Гб?')
+// let numb = Math.floor((Vol*1024)/820)
+// alert(numb)
+
+// //8 task 
+// let sum = prompt('Сколько денег у вас в кошельке?')
+// let price = prompt('Сколько стоит одна шоколадка?')
+// sum = Number(sum)
+// price = Number(price)
+// let sell = 
+
+// //9 task 
+// Number = prompt('Введите трехзначное число:')
+// let a = Number(nmb % 10)
+// let b = Number((nmb - a)/ 10 % 10)
+// let c = Number((nmb - a - b * 10)/ 100)
+// alert('Задом-наперед:' + (a * 100 + b * 10 + c))
+
+// //10 task 
+// let a = prompt('Введите число для проверки на четность: ');
+// a = Number(a);
